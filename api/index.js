32e1255013a0678909e7f3e266d5f2b3e8c1e26d
wujunfeng <wujf@8btc.com>
2023-02-15 16:42:45 +0800

    ss

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3008;
 
+app.get("/", (req, res) => {
+  res.send("index");
+});
 app.get("/api", (req, res) => {
   res.send("Hello World! 1111");
 });
