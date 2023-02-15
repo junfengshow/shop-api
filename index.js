08e847786eb67a179944b9a47811ecc952af1577
wujunfeng <wujf@8btc.com>
2023-02-15 16:28:25 +0800

    listen

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3008;
 
 app.get("/", (req, res) => {
   res.send("Hello World! 1111");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
 
 module.exports = app;
