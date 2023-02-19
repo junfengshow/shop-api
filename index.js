1e6f218aba6b4ca5a43e1ae28f6b51b53a2b6f27
wujunfeng <wujf@8btc.com>
2023-02-19 15:06:30 +0800

    express

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3008;
-
-app.get("/api", (req, res) => {
-  res.end("Hello World! 1111");
-});
-
-app.get("/", (req, res) => {
-  res.send("index");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+#!/usr/bin/env node
 
 // module.exports = app;
 // "rewrites": [{ "source": "/api/(.*)", "destination": "/api" }]
+const express = require("express");
+async function start() {
+  const app = express();
+  const port = process.env.PORT || 3008;
+
+  app.get("/api", (req, res) => {
+    res.end("Hello World! 1111");
+  });
+
+  app.get("/", (req, res) => {
+    res.send("index");
+  });
+  app.server = app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+  return app;
+}
+
+start();
